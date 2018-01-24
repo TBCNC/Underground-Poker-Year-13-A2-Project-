733944d7443507cd54d9a5581ddec31b88723b68
TBCNC <chasjohnh@gmail.com>
2018-01-24 16:59:43 +0000

    Fixed cycling issue in salt algorithm

--- SFML project/main.cpp
+++ SFML project/main.cpp
@@ -7,18 +7,7 @@
 #include <Windows.h>
 
 int main() {
-	srand(std::time(0));
-	std::string password1, password2;
-	std::cout << "Enter a password:";
-	std::cin >> password1;
-	password1 = PasswordHash::GeneratePassword(password1);
-	std::cout << password1 << std::endl;
-	std::cout << "Enter a password to see if it matches:";
-	std::cin >> password2;
-	if (PasswordHash::ComparePasswords(password2, password1))
-		std::cout << "They match!" << std::endl;
-	else
-		std::cout << "They don't match!" << std::endl;
+	
 	system("pause");
 }
  
